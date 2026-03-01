82bf08e682300ed1e4ce12cdfe5f6663cb005f1a
Saravanakannan12 <saro361@outlook.com>
2026-03-01 21:08:04 +0000

    Added composite key field in the repository

--- src/main/java/com/application/weather/metricsApp/Metric/Repository/MetricRepository.java
+++ src/main/java/com/application/weather/metricsApp/Metric/Repository/MetricRepository.java
@@ -1,11 +1,12 @@
 package com.application.weather.metricsApp.Metric.Repository;
 
 import com.application.weather.metricsApp.Metric.Metric;
+import com.application.weather.metricsApp.Metric.SensorMetricId;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface MetricRepository extends JpaRepository<Metric, Long> {
+public interface MetricRepository extends JpaRepository<Metric, SensorMetricId> {
 
 
 }
