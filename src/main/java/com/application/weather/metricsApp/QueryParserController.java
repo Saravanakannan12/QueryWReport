92e952dc22059e1b12765bcbc466a199459e98f3
Saravanakannan12 <saro361@outlook.com>
2026-03-01 00:37:13 +0000

    System prompt message added for building query.

--- src/main/java/com/application/weather/metricsApp/QueryParserController.java
+++ src/main/java/com/application/weather/metricsApp/QueryParserController.java
@@ -1,6 +1,5 @@
 package com.application.weather.metricsApp;
 
-import com.application.weather.metricsApp.Metric.QueryResult;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
@@ -19,15 +18,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/v1")
 public class QueryParserController {
 
-    //private String userQuery = "Give me the average temperature and humidity for sensor 1 in the last day";
-
     private final QueryParserService queryParserService;
 
     @GetMapping("/userQuery")
     public void parseUserQuery(@RequestBody @NotBlank(message = "Please enter a valid query") String userQuery) throws JsonProcessingException {
         log.info("User query {}", userQuery);
-        QueryResult queryResult = queryParserService.parseQuery(userQuery);
-        log.info("Query after LLM modified {}", queryResult.toString());
+        String queryResult = queryParserService.parseQuery(userQuery);
+
     }
 
 }
